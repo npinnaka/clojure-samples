96aff8b57d2599a54dcc3100bbcb47b2dc0326eb
Narendra Pinnaka <learnnew@outlook.com>
2018-07-02 09:58:40 -0500

    Update if_then_and_everything_else.clj

--- src/clojure_samples/if_then_and_everything_else.clj
+++ src/clojure_samples/if_then_and_everything_else.clj
@@ -3,15 +3,20 @@
   ;(:import)
   (:gen-class))
 
-(defn simple-if[a b]
+(defn simple-if
+  "function for if"
+  [a b]
   (if (> a b)
     (identity true)))
 
 
-(defn simple-if-else[a b]
+(defn simple-if-else
+  "function for if else"
+  [a b]
   (if (> a b)
     (identity true)
     (identity false)))
 
+;;function calls
 (simple-if 4 3)
 (simple-if-else 3 4)
