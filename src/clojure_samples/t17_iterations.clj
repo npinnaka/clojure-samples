b4ecae5a917c6501261b76890f22bc010202b404
Narendra Pinnaka <learnnew@outlook.com>
2018-07-04 22:56:13 -0500

    iteratatins added

--- src/clojure_samples/t17_iterations.clj
+++ src/clojure_samples/t17_iterations.clj
@@ -1,2 +1,16 @@
 (ns clojure-samples.t17-iterations)
 
+(defn recur-sum [num]
+  (if (= num 0)
+    0
+    (+ num (recur-sum (dec num)))))
+
+(defn tail-recur-sum [num]
+  (loop [sum          0
+         loop-counter num]
+    (if (= loop-counter 0)
+      sum
+      (recur (+ sum loop-counter) (dec loop-counter)))))
+
+(tail-recur-sum 100000)
+(recur-sum 100000)
